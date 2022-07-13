88c914185f08c16def4a2deb23bcb3d79dd27cd6
AlexSaxena <saxenaalex@gmail.com>
2022-07-13 22:51:38 +0200

    Imports > createToDo & Adds btn test functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import content from "./content";
 // import "./styles/main.css";
-
+import { createNewToDo, checkToDo } from "./createToDo";
 content;
 console.log("Greetings, General Kenobi!");
+
+document.querySelector(".create-items").addEventListener("click", () => {
+  createNewToDo("a", "a", "a", "top", true);
+});
+
+document.querySelector(".show-items").addEventListener("click", () => {
+  checkToDo();
+});
