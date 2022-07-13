f6b0e0f70d388513b63a4405215212afaee95146
AlexSaxena <saxenaalex@gmail.com>
2022-07-13 16:21:16 +0200

    Adds CreateNewToDo Func.. GenerateID Func.. Array to hold ToDo's

--- src/createToDo.js
+++ src/createToDo.js
@@ -1,3 +1,31 @@
 /*  factory Function to create new ToDo Items
-    Every object Should have | Title, Description, Due Date, Priority, Checkbox(True == Completed) |
+    Every object Should have | Title, Description, Due Date, Priority, Checkbox(True == Completed), Id |
+
+    For Testing
+    createNewToDo("a", "a", "a", "top", true);
+    let test1 = createNewToDo("b", "b", "b", "mid", true);
+    let test2 = createNewToDo("c", "c", "c", "bottom", false);
+
 */
+
+let id = 1;
+let allToDoArray = [];
+
+function generateId() {
+  let test = id++;
+  return test;
+}
+
+function createNewToDo(title, Desc, date, priority, completed = false) {
+  todo = {
+    Title: title,
+    Description: Desc,
+    DueDate: date,
+    Priority: priority,
+    Completed: completed,
+    Id: generateId(),
+  };
+  allToDoArray.push(todo);
+
+  return todo;
+}
