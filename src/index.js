5631aebab726ac20e3ddfdd8c13c5d333b9cb1a7
AlexSaxena <saxenaalex@gmail.com>
2022-07-21 14:52:25 +0200

    Adds ResetForm(), Resets Form Values

--- src/index.js
+++ src/index.js
@@ -7,32 +7,54 @@ content;
 let popup = document.getElementById("popup");
 // createNewToDo("a", "a", "a", "top", true);
 
-const renderVariables = (function () {
+const renderBodyElements = (function () {
   let openPopupBtn = document.querySelector(".btn-create-todo");
   let closePopupBtn = document.querySelector(".popup-btn-exit");
   let popup = document.getElementById("popup");
-  return { openPopupBtn, closePopupBtn, popup };
+  let formTitle = document.querySelector("#title");
+  let formDesc = document.querySelector("#description");
+  let formPriority = document.querySelector("#priority");
+  let formDueDate = document.querySelector("#duedate");
+  return {
+    openPopupBtn,
+    closePopupBtn,
+    popup,
+    formTitle,
+    formDesc,
+    formPriority,
+    formDueDate,
+  };
 })();
 
 // Event Listeners For opening & Closing Form
-renderVariables.openPopupBtn.addEventListener("click", () => {
+renderBodyElements.openPopupBtn.addEventListener("click", () => {
   if (popup.classList.contains("open-popup")) {
     closePopup();
+    resetForm();
   } else {
     showPopup();
   }
 });
 
-renderVariables.closePopupBtn.addEventListener("click", () => {
+const resetForm = () => {
+  renderBodyElements.formTitle.value = null;
+  renderBodyElements.formDesc.value = null;
+  renderBodyElements.formDueDate.value = null;
+  renderBodyElements.formPriority.value = "low";
+};
+
+// Listeners
+renderBodyElements.closePopupBtn.addEventListener("click", () => {
   closePopup();
+  resetForm();
 });
 
 // Functions for Removing Open/Close Popup CLASS
 const showPopup = () => {
-  renderVariables.popup.classList.add("open-popup");
+  renderBodyElements.popup.classList.add("open-popup");
 };
 const closePopup = () => {
-  renderVariables.popup.classList.remove("open-popup");
+  renderBodyElements.popup.classList.remove("open-popup");
 };
 
 // document.querySelector(".show-items").addEventListener("click", () => {
